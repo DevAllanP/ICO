76ef7ca94bc6e9b66ac0f4914bd4337499faa36a
Ap <allanparsy.dev@gmail.com>
2022-06-28 17:35:21 +0200

    ICO

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x0a4bde2a78a0a0516d205998d442c1104389241c";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
